8cab5d90fefc7986cfe17f9ae9e9a1348b163f1a
xHFx <HFYT@openmailbox.org>
2015-07-18 13:52:29 -0400

    initial summit

---
+++ src/Infocmd/MainClass.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Infocmd;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\event\Listener;
+use pocketmine\Player;
+use pocketmine\plugin\PluginBase;
+use pocketmine\Server;
+use pocketmine\utils\TextFormat;
+
+class MainClass extends PluginBase implements Listener{
+	public function onLoad(){
+		$this->getLogger()->info(TextFormat::WHITE . "InfoCmd has been loaded!");
+	}
+	public function onEnable(){
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+		$this->getLogger()->info(TextFormat::DARK_GREEN . "InfoCmd has been enabled!");
+     $this->saveDefaultConfig();
+    }
+	public function onDisable(){
+		$this->getLogger()->info(TextFormat::DARK_RED . "InfoCmd has been disabled!");
+	}
+    public function onCommand(CommandSender $sender, Command $command, $label, array $args){
+         switch($command->getName()){
+             case "info":
+                  $player = $event->getPlayer();
+             foreach($this->getConfig()->get("info") as $info){
+               $player->sendMessage("[Info]" . $info . "!");}
+                 return true;
+             default:
+                 return false;
+         }
